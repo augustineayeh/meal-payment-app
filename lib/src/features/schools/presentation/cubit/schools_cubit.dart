d35fcd43dfc9bd12d3b8e4d26bdeadd94e30a868
Augustine <augyog07@gmail.com>
2024-04-18 12:29:15 +0000

    add school cubit

---
+++ lib/src/features/schools/presentation/cubit/schools_cubit.dart
@@ -0,0 +1,76 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz_input/formz_input.dart';
+import '../../domain/entities/school.dart';
+import '../../domain/repositories/school_repository.dart';
+
+part 'schools_state.dart';
+
+class SchoolCubit extends Cubit<SchoolState> {
+  SchoolCubit({
+    required SchoolRepository schoolRepository,
+  })  : _schoolRepository = schoolRepository,
+        super(
+          const SchoolState(),
+        );
+
+  final SchoolRepository _schoolRepository;
+
+  void fetchSchoolsList() async {
+    emit(
+      state.copyWith(
+        status: SchoolStatus.loading,
+      ),
+    );
+
+    final failureOrSchoolsList = await _schoolRepository.fetchSchools();
+
+    failureOrSchoolsList.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: SchoolStatus.failure,
+            errorMessage: failure.errorMessage,
+          ),
+        );
+      },
+      (schoolsList) {
+        emit(
+          state.copyWith(
+            status: SchoolStatus.success,
+            schools: schoolsList,
+          ),
+        );
+      },
+    );
+  }
+
+  void fetchSingleSchool(String schoolId) async {
+    emit(
+      state.copyWith(
+        status: SchoolStatus.loading,
+      ),
+    );
+
+    final failureOrSchool = await _schoolRepository.fetchSingleSchool(schoolId);
+
+    failureOrSchool.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: SchoolStatus.failure,
+            errorMessage: failure.errorMessage,
+          ),
+        );
+      },
+      (school) {
+        emit(
+          state.copyWith(
+            status: SchoolStatus.success,
+            school: school,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/features/schools/presentation/cubit/schools_state.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
+
+part of 'schools_cubit.dart';
+
+enum SchoolStatus { initial, loading, success, failure }
+
+class SchoolState extends Equatable {
+  const SchoolState({
+    this.name = const Field.pure(),
+    this.location = const Field.pure(),
+    this.phoneNumber = const Field.pure(),
+    this.email = const Field.pure(),
+    this.status = SchoolStatus.initial,
+    this.school = School.empty,
+    this.schools = const <School>[],
+    this.errorMessage,
+  });
+  final Field name;
+  final Field location;
+  final Field phoneNumber;
+  final Field email;
+  final SchoolStatus status;
+  final School school;
+  final List<School> schools;
+  final String? errorMessage;
+
+  SchoolState copyWith({
+    Field? name,
+    Field? location,
+    Field? phoneNumber,
+    Field? email,
+    SchoolStatus? status,
+    School? school,
+    List<School>? schools,
+    String? errorMessage,
+
+  }) {
+    return SchoolState(
+      name: name ?? this.name,
+      location: location ?? this.location,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      email: email ?? this.email,
+      status: status ?? this.status,
+      school: school ?? this.school,
+      schools: schools ?? this.schools,
+      errorMessage: errorMessage ?? errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props {
+    return [
+      name,
+      location,
+      phoneNumber,
+      email,
+      status,
+      school,
+      schools,
+      errorMessage,
+    ];
+  }
+}

42a2e0f7326f749ba892ce48a6189ebe72e3f2c6
Augustine <augyog07@gmail.com>
2024-04-20 12:46:33 +0000

    add students cubit

--- lib/src/features/students/presentation/cubit/students_cubit.dart
+++ lib/src/features/students/presentation/cubit/students_cubit.dart
@@ -1,8 +1,77 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:formz_input/formz_input.dart';
+import '../../domain/entities/student.dart';
+import '../../domain/repositories/student_repository.dart';
 
 part 'students_state.dart';
 
-class StudentsCubit extends Cubit<StudentsState> {
-  StudentsCubit() : super(StudentsInitial());
+class StudentCubit extends Cubit<StudentState> {
+  StudentCubit({
+    required StudentRepository studentRepository,
+  })  : _studentRepository = studentRepository,
+        super(
+          const StudentState(),
+        );
+
+  final StudentRepository _studentRepository;
+
+  void fetchStudentsList() async {
+    emit(
+      state.copyWith(
+        status: StudentStatus.loading,
+      ),
+    );
+
+    final failureOrStudentsList = await _studentRepository.fetchStudents();
+
+    failureOrStudentsList.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: StudentStatus.failure,
+            errorMessage: failure.errorMessage,
+          ),
+        );
+      },
+      (studentsList) {
+        emit(
+          state.copyWith(
+            status: StudentStatus.success,
+            students: studentsList,
+          ),
+        );
+      },
+    );
+  }
+
+  void fetchSingleStudent(String studentId) async {
+    emit(
+      state.copyWith(
+        status: StudentStatus.loading,
+      ),
+    );
+
+    final failureOrStudent =
+        await _studentRepository.fetchSingleStudent(studentId);
+
+    failureOrStudent.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: StudentStatus.failure,
+            errorMessage: failure.errorMessage,
+          ),
+        );
+      },
+      (student) {
+        emit(
+          state.copyWith(
+            status: StudentStatus.success,
+            student: student,
+          ),
+        );
+      },
+    );
+  }
 }
--- lib/src/features/students/presentation/cubit/students_state.dart
+++ lib/src/features/students/presentation/cubit/students_state.dart
@@ -1,10 +1,74 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+
 part of 'students_cubit.dart';
 
-abstract class StudentsState extends Equatable {
-  const StudentsState();
+enum StudentStatus { initial, loading, success, failure }
+
+class StudentState extends Equatable {
+  const StudentState({
+    this.firstName = const Field.pure(),
+    this.lastName = const Field.pure(),
+    this.middleName = const Field.pure(),
+    this.fullName = const Field.pure(),
+    this.status = StudentStatus.initial,
+    this.schoolName = const Field.pure(),
+    this.schoolId = const Field.pure(),
+    this.student = Student.empty,
+    this.students = const <Student>[],
+    this.errorMessage,
+  });
+
+  final Field firstName;
+  final Field lastName;
+  final Field middleName;
+  final Field fullName;
+  final Field schoolName;
+  final Field schoolId;
+  final StudentStatus status;
+  final Student student;
+  final List<Student> students;
+  final String? errorMessage;
+
+  StudentState copyWith({
+    Field? firstName,
+    Field? lastName,
+    Field? middleName,
+    Field? fullName,
+    Field? schoolName,
+    Field? schoolId,
+    StudentStatus? status,
+    Student? student,
+    List<Student>? students,
+    String? errorMessage,
+  }) {
+    return StudentState(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      middleName: middleName ?? this.middleName,
+      fullName: fullName ?? this.fullName,
+      schoolName: schoolName ?? this.schoolName,
+      schoolId: schoolId ?? this.schoolId,
+      status: status ?? this.status,
+      student: student ?? this.student,
+      students: students ?? this.students,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props {
+    return [
+      firstName,
+      lastName,
+      middleName,
+      fullName,
+      schoolName,
+      schoolId,
+      status,
+      student,
+      students,
+      errorMessage,
+    ];
+  }
 }
-
-class StudentsInitial extends StudentsState {}

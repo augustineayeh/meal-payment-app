b23229e9db77a3a359eb218b8ffc0ae9ff7765c8
Augustine <augyog07@gmail.com>
2024-04-24 13:19:33 +0000

    add providers

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'src/features/auth/domain/repository/repository.dart';
+import 'src/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
+import 'src/features/vendors/presentation/pages/vendor_home_page.dart';
 
-import 'src/features/schools/presentation/pages/schools_page.dart';
+import 'src/core/service_locator/service_locator.dart';
+import 'src/features/schools/domain/repositories/school_repository.dart';
+import 'src/features/schools/presentation/cubit/schools_cubit.dart';
+import 'src/features/vendors/domain/repositories/vendor_repository.dart';
+import 'src/features/vendors/presentation/cubit/vendors_cubit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Meal payment app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SchoolCubit>(
+          create: (context) => SchoolCubit(
+            schoolRepository: locator<SchoolRepository>(),
+          ),
+        ),
+        BlocProvider<SignupCubit>(
+          create: (context) => SignupCubit(
+            authenticationRepository: locator<AuthenticationRepository>(),
+          ),
+        ),
+        BlocProvider<VendorCubit>(
+          create: (context) => VendorCubit(
+            vendorRepository: locator<VendorRepository>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Meal payment app',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const VendorHomePage(),
       ),
-      home: const SchoolsPage(),
     );
   }
 }
